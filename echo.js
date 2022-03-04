ebfa10503f952cbf09ea8af3453284e4067fd80c
burak-upstash <burak@upstash.com>
2022-03-04 13:50:43 +0300

    x

--- echo.js
+++ echo.js
@@ -1,4 +1,5 @@
 export default function handler(request, response) {
-    const {name} = request.query;
-    response.status(200).send(`Hello ${name}`);
+    console.log(request.query)
+    // const { name } = request.query;
+    response.status(200).send(`Hello Ali!`);
 }
